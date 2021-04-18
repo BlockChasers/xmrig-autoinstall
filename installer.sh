5e1b84ddb17c0acf4f01e276efab246918dbc11a
MooCat <43627985+leshacat@users.noreply.github.com>
2021-04-18 01:10:54 -0400

    Fixed for HiveOS (temp removed libssl-dev)
    temp removed libssl-dev will put back later with flags or distro-detection

--- installer.sh
+++ installer.sh
@@ -7,7 +7,9 @@ apt upgrade -y ; apt dist-upgrade -y ;
 
 echo "INSTALLING REQ'D PACKAGES"
 
-apt install -y htop cpulimit git build-essential cmake libuv1-dev libssl-dev libhwloc-dev automake libtool autoconf ;
+# libssl-dev (REMOVED FOR HIVEOS)
+
+apt install -y htop cpulimit git build-essential cmake libuv1-dev libhwloc-dev automake libtool autoconf ;
 
 echo "CREATING DIRECTORIES"
 
@@ -43,7 +45,7 @@ echo "Type=simple" | tee -a /etc/systemd/system/xmrig.service
 echo "Restart=always" | tee -a /etc/systemd/system/xmrig.service
 echo "RestartSec=3" | tee -a /etc/systemd/system/xmrig.service
 echo "User=root" | tee -a /etc/systemd/system/xmrig.service
-echo "ExecStart=/root/MINERS/xmrig/xmrig --coin monero --tls -o 192.168.0.52:33333 -u ${HOSTNAME} -p ${HOSTNAME} --log-file=/var/log/xmrig.log --donate-level=0" | tee -a /etc/systemd/system/xmrig.service
+echo "ExecStart=/root/MINERS/xmrig/xmrig --coin monero -o 192.168.0.52:33333 -u ${HOSTNAME} -p ${HOSTNAME} --log-file=/var/log/xmrig.log --donate-level=0" | tee -a /etc/systemd/system/xmrig.service
 echo "" | tee -a /etc/systemd/system/xmrig.service
 echo "[Install]" | tee -a /etc/systemd/system/xmrig.service
 echo "WantedBy=multi-user.target" | tee -a /etc/systemd/system/xmrig.service
